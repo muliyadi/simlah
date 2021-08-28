acca5dc6db8d8e2381b25ac1697189f51c91ac81
muliyadi <31653250+muliyadi@users.noreply.github.com>
2021-08-28 08:18:57 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,21 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th>a</th>
+            <th>b</th>
+            <th>c</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td scope="row"></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td scope="row"></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
